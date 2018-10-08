27614362fb0195d0fdfebebd2b93b0984e391a58
Dominick Schroer <dominick@schroer.ca>
2018-10-08 11:08:45 -0400

    Added loading to the post list

--- src/post-list.tsx
+++ src/post-list.tsx
@@ -2,33 +2,31 @@ import React, { CSSProperties } from "react";
 import { Post } from "./post";
 import { IPost } from "./models/post";
 import { PostService } from "./services/post-service";
+import { loading } from "./utils/loading-bar";
 
 interface IFormState {
     postService: PostService;
 }
 
 interface IListState {
+    loading: boolean;
     posts: IPost[];
 }
 
 export class PostList extends React.Component<IFormState, IListState> {
     constructor(props: Readonly<IFormState>) {
         super(props);
-        this.state = { posts: [] };
+        this.state = { posts: [], loading: true };
 
         props.postService.posts.subscribe(posts => {
-            this.setState({ posts: posts });
+            this.setState({ posts: posts, loading: false });
         });
     }
 
     public render() {
-        const style: CSSProperties = {
-            position: "absolute",
-        };
-
         return (
             <div>
-                <div style={style}>
+                <div>
                     {this.posts()}
                 </div>
             </div>
@@ -36,6 +34,10 @@ export class PostList extends React.Component<IFormState, IListState> {
     }
 
     public posts() {
+        if (this.state.loading) {
+            return loading();
+        }
+
         let previous: Date;
         return this.state.posts.map(post => {
             const render = (<Post post={post} key={post.id} previous={previous}></Post>);
